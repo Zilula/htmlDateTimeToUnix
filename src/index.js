d935193627155bde95756aa26ae8118e66ed876c
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-05-09 13:39:19 -0700

    added testing coverage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const getUnixTime = (date, time) => {
   const unix = new Date(date + ' ' + time);
+  if(isNaN(unix)) {
+    throw 'NaN both date and time must be strings';
+  }
   return unix.getTime();
 };
 export default getUnixTime;
--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,22 @@
 import getUnixTime from '../src/index';
 
-
 describe('tests default getUnixTime fn', () => {
-  const date = '2019-05-29';
-  const time = '12:03';
   it('calls function', () => {
+    const date = '2019-05-29';
+    const time = '12:03';
     const result = getUnixTime(date, time);
     expect(result).toEqual(1559156580000);
+  });
+
+  it('errors when date is not passed as a string', () => {
+    const date = 20190529;
+    const time = '12:03';
+    expect(() => getUnixTime(date, time)).toThrowError('NaN both date and time must be strings');
+  });
+
+  it('errors when time is not passed as a string', () => {
+    const date = '2019-05-29';
+    const time = 1203;
+    expect(() => getUnixTime(date, time)).toThrowError('NaN both date and time must be strings');
   });
 });
