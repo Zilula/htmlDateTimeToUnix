39d3f05ca8747f36d40fac0bcefe38e64dd920ea
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-05-09 11:37:39 -0700

    config for babel-eslint and config for testing suite

--- test/index.js
+++
@@ -1,9 +0,0 @@
-// import { assert } from 'chai';
-// import getUnixTime from '../src/index';
-
-// describe('Awesome test.', () => {
-//   it('should test default getUnixTime function', () => {
-//     const expectedVal = 'I am the Default Awesome Function, fellow comrade! - Dinesh';
-//     assert(getUnixTime('Dinesh') === expectedVal, 'Default not awesome :(');
-//   });
-// });
---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+// import getUnixTime from '../src/index';
+
+describe('Awesome test.', () => {
+  it('should test default getUnixTime function', () => {
+    expect(true).toEqual(true);
+  });
+});
