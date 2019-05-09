fea9d5620eb4daaa737479486ac1619d63f643f2
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-05-09 13:41:47 -0700

    whoops

--- test/index.test.js
+++ test/index.test.js
@@ -5,7 +5,7 @@ describe('tests default getUnixTime fn', () => {
     const date = '2019-05-29';
     const time = '12:03';
     const result = getUnixTime(date, time);
-    expect(result).toEqual(1559156580000);
+    expect(result).toEqual(expect.any(Number));
   });
 
   it('errors when date is not passed as a string', () => {
